1e150544cdd600e1b7fb451f27d120fc55410e30
ImperialOfficer <nolancwong@gmail.com>
2025-11-07 13:50:45 -0600

    CSS fix FINAL... For now at least

--- Wong-contact.js
+++ Wong-contact.js
@@ -2,19 +2,14 @@ let submitButton = document.querySelector(".submit");
 let messages = document.querySelector(".comms").value;
 let usrName=document.querySelector(".usrName").value;
 let thank=document.querySelector(".thank");
-let nav3=document.querySelector(".navbar-con3");
+let nav=document.querySelector(".contact");
 let eMail=document.querySelector(".eMail").value;
 // Displays thanks message and resets navbar
 submitButton.addEventListener("click", function() {
 let eMail_value=eMail.value;
 let messages_value=messages.value;
-let usrName_value=usrName.value
-thank.style.display="block"
-console.log(usrName_value)
-if (nav3.style.marginTop==19.4){
-nav3.style.marginTop="16.5%"
-}
-else{
-    nav3.style.marginTop="22.5%"
-}
-});
+let usrName_value=usrName.value;
+thank.style.display="block";
+console.log(usrName_value);
+nav.style.paddingBottom="11.1%";
+})
